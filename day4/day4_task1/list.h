28e71e98cae3cfd29505814dd7f9eb024052aa77
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-12-05 22:10:46 +0100

    wip

--- day4/day4_task1/list.h
+++ day4/day4_task1/list.h
@@ -11,45 +11,59 @@
 	typedef struct {			\
 		struct n_##TYPE *first;	\
 		struct n_##TYPE *last;	\
+		size_t size;\
 	}					\
 	l_##TYPE;				\
 \
 	struct n_##TYPE*			\
 	new_n_##TYPE(void)			\
 	{					\
-		struct n_##TYPE *new = (struct n_##TYPE *) 	\
-			malloc(sizeof(struct n_##TYPE));		\
-		return new;						\
+		return (struct n_##TYPE *) malloc(sizeof(struct n_##TYPE));	\
 	}								\
+\
+	char	\
+	is_empty_##TYPE(const l_##TYPE *l)\
+	{		\
+		return (l->size < 1);	\
+	}			\
 \
 	int								\
 	append_back_##TYPE(l_##TYPE *l, TYPE n)				\
 	{								\
 		struct n_##TYPE *new = new_n_##TYPE();		\
 		if (new == NULL || l == NULL)			\
-			return 1;					\
+			return -1;					\
 		new->data = n;						\
 		new->prev = l->last;					\
 		new->next = NULL;					\
-		if (l->first == NULL && l->last == NULL)		\
+		if (is_empty_##TYPE(l))		\
 			l->first = new;				\
 		else							\
 			l->last->next = new;				\
 		l->last = new;					\
-		return 0;						\
+		return ++l->size;				\
 	}								\
+\
+	TYPE							\
+	pop_first_##TYPE(l_##TYPE *l)			\
+	{					\
+		struct n_##TYPE *f = l->first;	\
+		l->first = f->next;	\
+		if (l->first != NULL)\
+			l->first->prev = NULL;		\
+		else\
+			l->last = NULL;\
+		--l->size;\
+		const TYPE ret = f->data;	\
+		free(f);					\
+		return ret;					\
+	}\
 \
 	void								\
 	destroy_##TYPE(l_##TYPE *l)					\
 	{								\
-		for (struct n_##TYPE *n = l->first; n != NULL;) {\
-            struct n_##TYPE *tmp = n->next;\
-            free(n);						\
-            n = tmp;                        \
-        }                               \
-        l->first = NULL;              \
-        l->last = NULL;              \
+		while (!is_empty_##TYPE(l))\
+			pop_first_##TYPE(l);\
 	}
-
 
 #endif
--- day4/day4_task1/main.c
+++ day4/day4_task1/main.c
@@ -3,16 +3,22 @@
 #include "list.h"
 #define BINGO_DIM 5
 
-GENERATE_LIST(int)
-GENERATE_LIST(l_int)
+typedef struct {
+	int num;
+	char b;
+}
+intbool;
+
+
+GENERATE_LIST(intbool)
+GENERATE_LIST(l_intbool)
 
 void
-read_int_line_into_list(FILE *f, l_int *list, const char* formatting)
+read_int_line_into_list(FILE *f, l_intbool *list, const char* formatting)
 {
-	int n = 0;
-	while (fscanf(f, formatting, &n) > 0) {
-		
-		append_back_int(list, n);
+	intbool n = {.num = 0, .b = 0};
+	while (fscanf(f, formatting, &(n.num)) > 0) {
+		append_back_intbool(list, n);
 		const char c = getc(f);
 		if (c == '\n')
 			break;
@@ -21,45 +27,67 @@ read_int_line_into_list(FILE *f, l_int *list, const char* formatting)
 }
 
 void
-read_bingo(FILE *f, l_l_int *list) {
+read_bingo(FILE *f, l_l_intbool *list)
+{
 	char c = '\0';
 	while (1) {
 		c = getc(f);
-		if (c == EOF) {
+		if (c == EOF)
 			break;
-		}
 		ungetc(c, f);
 
-		l_int tmp = {.first = NULL, .last = NULL};
-		for (int i = 0; i < BINGO_DIM; ++i)
-		{
-			
+		l_intbool tmp = {.first = NULL, .last = NULL};
+		for (int i = 0; i < BINGO_DIM; ++i) {
 			read_int_line_into_list(f, &tmp, "%i");
 		}
-		append_back_l_int(list, tmp);
+		append_back_l_intbool(list, tmp);
 	}
 }
 
-int main (void) {
+int check_bingos(l_l_intbool *l, const int num)
+{
+	for (struct n_l_intbool *n = l->first; n != NULL; n = n->next) {
+		for (struct n_intbool *m = n->data.first; m != NULL; m = m->next) {
+			m->data.b = (num == m->data.num);
+			printf("%i\n", (num == m->data.num));
+		}
+	}
+}
+
+int check_hor_ver(l_intbool *l) {
+	int i = 0;
+	for (struct n_intbool *n = l->first; n != NULL; n = n->next, ++i) {
+		
+	}
+}
+
+int main (void) 
+{
 //open file
 	FILE *f = fopen("input.txt", "r");
 	if (f == NULL)
 		return 1;
 //read first line into list
-	l_int list = {.first = NULL, .last = NULL};
+	l_intbool list = {.first = NULL, .last = NULL, .size = 0};
 	read_int_line_into_list(f, &list, "%u,");
-	l_l_int l = {.first = NULL, .last = NULL};
-	read_bingo(f, &l);
+	l_l_intbool bingo = {.first = NULL, .last = NULL};
+	read_bingo(f, &bingo);
+	while (!is_empty_intbool(&list))
+	{
+		check_bingos(&bingo, pop_first_intbool(&list).num);
+	}
+	
 
-//DEBUG display first line
-	for (struct n_l_int* n = l.first; n != NULL; n = n->next) {
-		for (struct n_int* m = n->data.first; m != NULL; m = m->next) {
-			printf("%i ", m->data);
+
+//DEBUG display bingo
+	while(!is_empty_l_intbool(&bingo)) {
+		l_intbool m = pop_first_l_intbool(&bingo);
+		while(!is_empty_intbool(&m)) {
+			intbool ib =  pop_first_intbool(&m);
+			printf("%i %i\n", ib.num, ib.b);
 		}
-			
 		puts("");
 	}
-	destroy_int(&list);
-	destroy_l_int(&l);
+	destroy_intbool(&list);
 	fclose(f);
 }

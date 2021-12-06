1afca896758a96fb8e53320fe32076c98a98106f
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-12-06 19:47:01 +0100

    day6 part 2 done!

--- day6/day6_task1.py
+++ day6/day6_task1.py
@@ -1,9 +1,8 @@
 fish = [int(x) for x in open("input.txt").read().split(',')]
 
 for day in range(80):
-    fish = list(map(lambda x : x - 1, fish))
-    new_fish = len(fish) - len(tuple(filter(lambda x : x > -1, fish)))
-    fish = list(map(lambda x : 6 if x < 0 else x, fish))
-    fish += [8] * new_fish
+    new_fish = len(tuple(filter(lambda x : x == 0, fish)))
+    fish = list(map(lambda x : 6 if x - 1 < 0 else x - 1, fish))
+    fish.extend([8] * new_fish)
 
 print(len(fish))

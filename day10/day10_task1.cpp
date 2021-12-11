6478ab644d8f2be9601673aa1ec4608a1782e9a3
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-12-11 03:43:08 +0100

    day 10 part 2 not done yet

--- day10/day10_task1.cpp
+++ day10/day10_task1.cpp
@@ -1,35 +1,30 @@
 #include <fstream>
 #include <vector>
-#include <array>
 #include <iostream>
 #include <algorithm>
 #include <map>
 
-int main (void) {
-    std::fstream f("input.txt");
-    std::vector<std::string> lines;
-    for (std::string line; getline(f, line);)
-        lines.push_back(line);
+using namespace std;
 
-    const std::map<char, char> m{{'}', '{'}, {']', '['}, {')', '('}, {'>', '<'}};
-    const std::array<char, 4> r_brk{'}',')','>',']'};
-    const std::map<char, int> vals{{')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}};
+int main (void) {
+    const map<char, char> expected{{'}', '{'}, {']', '['}, {')', '('}, {'>', '<'}};
+    const string r_brk = "})>]";
+    const map<char, int> vals{{')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}};
+
+    fstream f("input.txt");
     int p = 0;
-    for (std::string s : lines) {
-        std::vector<char> stack;
-        for (char c : s) {
-            if (stack.size() > 1 && std::find(r_brk.begin(),r_brk.end(), c) != r_brk.end()) {
-                if (stack.back() != m.at(c)) {
+    for (string s; getline(f, s);) {
+        vector<char> stack;
+        for (char c : s)
+            if (stack.size() > 1 && find(r_brk.begin(),r_brk.end(), c) != r_brk.end()) {
+                if (stack.back() != expected.at(c)) {
                     p += vals.at(c);
                     break;
-                } else {
-                    stack.pop_back();
                 }
-            } else {
+                stack.pop_back();
+            } else
                 stack.push_back(c);
-            }
-        }
-
     }
-    std::cout << p << '\n';
+    cout << p << '\n';
+    return 0;
 }
